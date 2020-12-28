b8e94fa1796501208a5f285e0cb622fb995db62f
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-12-28 13:53:14 +0700

    Change icon and site title

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_LOCATION: "http://192.168.0.101:5000/api",
+  API_LOCATION: "http://localhost:5000/api",
   SHOP_NAME: "KHARISMA ABADI",
   SHOP_ADDRESS: "Jl. Cendrawasih No.29 Rt.03 Rw.05, Genteng Kulon, Banyuwangi",
 };
